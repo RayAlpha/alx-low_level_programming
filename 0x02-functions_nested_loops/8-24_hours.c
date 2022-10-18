a1614f621c7a506667a575493068a48d5213368b
RayAlpha <onuoharaymond7@gmail.com>
2022-10-18 23:51:08 +0100

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,20 +3,38 @@
 /**
  * jack_bauer - prints every minute of the day
  */
-void jack_bauer(void)
+void jack_bauer(voi)
 {
-	int hour, minute;
+	int i, j;
 
-	for (hour = 0; hour <= 24; hour++)
+	for (i = 0; i < 24; i++)
 	{
-		for (minute = 0; minute <= 60; minute++)
+		for (j = 0; j < 60; j++)
 		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
-			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minute / 10) + '0');
+			if (i < 10)
+			{
+				_putchar('0');
+				_putchar(i + '0');
+			}
+			else if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			if (j < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(j + '0');
+			}
+			else if (j >= 10)
+			{
+				_putchar(':');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
 			_putchar('\n');
 		}
+
 	}
 }
