f3e211ce0d8d22d07197e30fa64252a8cd6643ee
RayAlpha <onuohaRaymond7@gmail.com>
2022-10-18 22:10:02 +0100

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,6 +4,7 @@
  * main - print _putchar
  * Return: 0
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -15,6 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
 	return (0);
 }
